f04bd9b71d86574cbc21974aa85241cad9dd484b
Sneha magtarpara <snehamagtarpara@gmail.com>
2023-01-02 05:57:16 +0530

    Initial Commit

---
+++ collection view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collection view
+//
+//  Created by R93 on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
